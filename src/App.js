6a15dec53127045cbd49684c25c4e856c4cccc77
jmoua123 <nraugnaagmouaa@gmail.com>
2022-10-24 21:02:47 -0700

    Images Corrected

--- src/App.js
+++ src/App.js
@@ -3,103 +3,107 @@ import './App.css'
 
 
 const Container = styled.div`
- display: flex; 
+ display: inline; 
  padding: 0px; 
- margin:25px 50px 75px 100px;
-`;
+ margin: 25px 50px 75px 100px;
+
+ `;
 
 const Column1 = styled.div`
 grid-template-columns: auto auto auto auto;
+
+@media screen (max-width 768ox) {
+  flex-grow: unset;
+ }
+ 
 `;
 const Column2 = styled.div`
 grid-template-columns: auto auto auto auto;
+
+@media screen (max-width 768ox) {
+  flex-grow: unset;
+ }
+ 
 `;
 const Column3 = styled.div`
 grid-template-columns: auto auto auto auto;
+
+@media screen (max-width 768ox) {
+  flex-grow: unset;
+ }
 `;
+
 const Leo = styled.div`
 height: 150px;
 width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Aries = styled.div`
 height: 150px;
 width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Taurus = styled.div`
 height: 150px;
 width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Gemini = styled.div`
 height: 150px;
 width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Cancer = styled.div`
 height: 150px;
 width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Virgo = styled.div`
 height: 150px;
 width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Libra = styled.div`
 height: 150px;
-width: 265px;
+width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Scorpius = styled.div`
 height: 150px;
 width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Sagittarius = styled.div`
 height: 150px;
 width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Capricornus = styled.div`
 height: 150px;
 width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Aquarius = styled.div`
 height: 150px;
 width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 const Pisces = styled.div`
 height: 150px;
-width: 240px;
+width: 254px;
 float: left;
-z-index: 1;
-padding: 20px 0;
+padding: 25px 0;
 `;
 
 
